6893549573f3e2e0f2b7c4e0bdc9cbd14dd153f2
We Connect <we>
2019-08-13 15:11:00 +0200

    Initial Commit

---
+++ TestApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestApp
+//
+//  Created by WeConnect on 13.08.19.
+//  Copyright © 2019 WeConnect. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
